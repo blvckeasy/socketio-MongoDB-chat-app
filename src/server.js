9523a0a5188ec12d732c27a1403093200382c438
Islom Abdurakhmonov <islomabdurakhmonov8@gmail.com>
2022-10-31 18:34:59 +0500

    helmet and morgan packages added v1

--- src/server.js
+++ src/server.js
@@ -3,7 +3,11 @@ import Express from 'express'
 import { Server as Socket } from 'socket.io'
 import { mongooseConnect } from './database/mongoose.js';
 import todoRoutes from './routes/user.js'
-
+import helmet from 'helmet'
+import morgan from 'morgan'
+import Fs from 'fs'
+import path from 'path'
+import { getCurrentDate } from './helpers/date.js'
 
 async function bootstrap() {
   const HOST = "localhost";
@@ -11,12 +15,18 @@ async function bootstrap() {
 
   const app = Express()
   const server = Http.createServer(app)
+  const accessLogStream = Fs.createWriteStream(path.join(process.cwd(), 'src', 'logs', `${getCurrentDate()}.log`))
   const io = new Socket(server, {
     cors: {
       origin: '*'
     }
   })
   
+  app.use(helmet())
+  // logger
+  app.use(morgan('combined', {
+    stream: accessLogStream
+  }))
   app.use(Express.json())
   app.use(todoRoutes);
 
