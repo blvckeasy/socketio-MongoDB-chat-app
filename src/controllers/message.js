31a8923264f0c34600c165709ed85529175573e3
Islom Abdurakhmonov <islomabdurakhmonov8@gmail.com>
2022-11-06 20:04:56 +0500

    error handler v1.0

--- src/controllers/message.js
+++ src/controllers/message.js
@@ -11,88 +11,104 @@ export default class MessageController {
   }
 
   async getMessages(req, res, next) {
-    const { to_user_id, from_user_id, id } = req.query
+    try {
+      const { to_user_id, from_user_id, id } = req.query
 
-    if (to_user_id && from_user_id) {
-      const messages = await this.Message.find({ from_user_id, to_user_id })
+      if (to_user_id && from_user_id) {
+        const messages = await this.Message.find({ from_user_id, to_user_id })
+        return res.send(messages || [])
+      }
+      if (id) {
+        const message = await this.Message.findOne({ _id: id }).clone()
+        return res.send(message || {})
+      }
+
+      const messages = await this.Message.find().clone()
       return res.send(messages || [])
+    } catch (error) {
+      next(error);
     }
-    if (id) {
-      const message = await this.Message.findOne({ _id: id }).clone()
-      return res.send(message || {})
-    }
-
-    const messages = await this.Message.find().clone()
-    return res.send(messages || [])
   }
 
   async postMessage(req, res, next) {
-    const { from_user_id, to_user_id, message } = req.body
-    if (!(from_user_id && to_user_id && message))
-      return res.send('req body null')
-
-    const select_users =
-      (await this.User.findOne({
-        $or: [{ _id: from_user_id }, { _id: to_user_id }],
-      })) || []
-    // it is necessary to check that from_user_id and to_user_id are in the database
-    if (select_users.length < 2) {
-      return res.send(
-        'from_user_id or to_user_id was not found in the database'
-      )
+    try {
+      const { from_user_id, to_user_id, message } = req.body
+      if (!(from_user_id && to_user_id && message))
+        return res.send('req body null')
+  
+      const select_users =
+        (await this.User.findOne({
+          $or: [{ _id: from_user_id }, { _id: to_user_id }],
+        })) || []
+      // it is necessary to check that from_user_id and to_user_id are in the database
+      if (select_users.length < 2) {
+        return res.send(
+          'from_user_id or to_user_id was not found in the database'
+        )
+      }
+  
+      const newMessage = await this.Message.create({
+        from_user_id,
+        to_user_id,
+        message,
+      })
+  
+      return res.status(201).send({
+        status: 201,
+        message: 'Message successfully created.',
+        data: newMessage,
+      })
+    } catch (error) {
+      next(error);
     }
-
-    const newMessage = await this.Message.create({
-      from_user_id,
-      to_user_id,
-      message,
-    })
-
-    return res.status(201).send({
-      status: 201,
-      message: 'Message successfully created.',
-      data: newMessage,
-    })
   }
 
   async editMessage(req, res, next) {
-    const { id } = req.params
-    const { message } = req.body
-
-    const { error, data: updated_message } = await new Promise(resolve => {
-      this.Message.findOneAndUpdate(
-        { _id: id },
-        { message },
-        function (err, data) {
-          return resolve(err, data)
-        }
-      )
-    })
-
-    if (error) return next(new MongooseInvalidDataError(error))
-
-    return res.send({
-      ok: true,
-      message: 'message successfully edited.',
-      data: updated_message,
-    })
+    try {
+      const { id } = req.params
+      const { message } = req.body
+  
+      const { error, data: updated_message } = await new Promise(resolve => {
+        this.Message.findOneAndUpdate(
+          { _id: id },
+          { message },
+          function (err, data) {
+            return resolve(err, data)
+          }
+        )
+      })
+  
+      if (error) return next(new MongooseInvalidDataError(error))
+  
+      return res.send({
+        ok: true,
+        message: 'message successfully edited.',
+        data: updated_message,
+      })
+    } catch (error) {
+      next(error);
+    }
   }
 
   async deleteMessage(req, res, next) {
-    const { id } = req.params
-    let { error, data: deleted_message } = await new Promise((resolve) => {
-      this.Message.findOneAndDelete({ _id: id }, function (err, data) {
-        return resolve({error: err, data});
+    try {
+      const { id } = req.params
+      let { error, data: deleted_message } = await new Promise((resolve) => {
+        this.Message.findOneAndDelete({ _id: id }, function (err, data) {
+          return resolve({error: err, data});
+        })
       })
-    })
-    
-    return next(new Error("hello world"));
-    if (error) return next(new MongooseInvalidDataError(error))
-
-    return res.status(200).send({
-      ok: true,
-      message: 'message successfully deleted.',
-      data: deleted_message,
-    })
+      
+      
+      if (error) return next(new MongooseInvalidDataError(error))
+  
+      return res.status(200).send({
+        ok: true,
+        message: 'message successfully deleted.',
+        data: deleted_message,
+      })
+    } catch (error) {
+      next(error);
+    }
   }
 }
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,206 +1,228 @@
-
-
 export default class UsersController {
   constructor(UserRepository) {
     this.User = UserRepository;
   }
 
   async getUsers(_, res) {
-    const users = await this.User.find().clone();
-    users.map((user) => user.password = undefined)
-    return res.send({
-      status: 200,
-      ok: true,
-      message: "OK.",
-      data: users || []
-    })
+    try {
+      const users = await this.User.find().clone();
+      users.map((user) => user.password = undefined)
+      return res.send({
+        status: 200,
+        ok: true,
+        message: "OK.",
+        data: users || []
+      })
+    } catch (error) {
+      next(error);
+    }
   }
 
   async getUser(req, res) {
-    const { id } = req.params
-    if (!id) return res.send("id is require!");
-
-    const user = await this.User.findOne({ _id: id });
-    user.password = undefined
-
-    if (user) {
+    try {
+      const { id } = req.params
+      if (!id) return res.send("id is require!");
+  
+      const user = await this.User.findOne({ _id: id });
+      user.password = undefined
+  
+      if (user) {
+        return res.send({
+          status: 200,
+          ok: true,
+          message: "user found.",
+          data: user
+        })
+      }
+      
       return res.send({
-        status: 200,
-        ok: true,
-        message: "user found.",
-        data: user
+        status: 404,
+        ok: false,
+        message: "user not found.",
+        data: {},
       })
+    } catch (error) {
+      next(error);
     }
-    
-    return res.send({
-      status: 404,
-      ok: false,
-      message: "user not found.",
-      data: {},
-    })
   }
 
   async login(req, res) {
-    const { username, password } = req.body;
-    if (!(username && password)) return res.send(CustomError.UnAuthorizationError());
-    
-    new Promise(async (resolve, reject) => {
-      await this.User.findOne({ username }, function (err, user) {
-        if (err) throw err;
-        if (!user) {
-          return reject();
+    try {
+      const { username, password } = req.body;
+      if (!(username && password)) return res.send(CustomError.UnAuthorizationError());
+      
+      new Promise(async (resolve, reject) => {
+        await this.User.findOne({ username }, function (err, user) {
+          if (err) throw err;
+          if (!user) {
+            return reject();
+          }
+  
+          user.comparePassword(password, function(err, isMatch) {
+              if (err) throw err;
+  
+              if (isMatch) {
+                return resolve(user)
+              }
+              return reject(CustomError.UnAuthorizationError())
+          });
+        }).clone();
+      }).then(async (user) => {
+        if (user) {
+          user.password = undefined
+  
+          return res.send({
+            status: 200,
+            ok: true,
+            message: "The user successfully finded.",
+            data: user
+          });
+        } else {
+          return {
+            status: 404,
+            ok: false,
+            message: "The user is not registered.",
+            data: {}
+          }
         }
-
-        user.comparePassword(password, function(err, isMatch) {
-            if (err) throw err;
-
-            if (isMatch) {
-              return resolve(user)
-            }
-            return reject(CustomError.UnAuthorizationError())
-        });
-      }).clone();
-    }).then(async (user) => {
-      if (user) {
-        user.password = undefined
-
-        return res.send({
-          status: 200,
-          ok: true,
-          message: "The user successfully finded.",
-          data: user
-        });
-      } else {
-        return {
-          status: 404,
-          ok: false,
-          message: "The user is not registered.",
-          data: {}
-        }
-      }
-    }).catch((err) => {
-      console.log('err', err)
-      return res.send({
-        status: 400,
-        ok: false,
-        error: err,
-      })
-    })
-  }
-
-  async register(req, res) {
-    const { username, password } = req.body;
-    if (!(username && password)) return res.send(CustomError.UnAuthorizationError());
-
-    const found_user = await this.User.findOne({ username })
-    
-    if (!found_user) {
-      try {
-        const user = await this.User.create({ username, password })
-        return res.send({
-          status: 200,
-          ok: true,
-          message: "user successfully registered.",
-          data: user
-        });
-      } catch (error) {
-        console.log(error);
+      }).catch((err) => {
+        console.log('err', err)
         return res.send({
           status: 400,
           ok: false,
-          error,
-          data: {}
-        })  
-      }
+          error: err,
+        })
+      })
+    } catch (error) {
+      next(error);
     }
-
-    return res.send({
-      status: 400,
-      ok: false,
-      message: `"${username}" is already sign in`,
-      user: {}
-    })
   }
 
-  async update(req, res) {
-    const { id } = req.params
-    if (!id) return res.send("id not defined");
-    const { username, password, old_password } = req.body
-    
-    if (!old_password) {
-      return res.send({
-        status: 400,
-        ok: false,
-        message: "old_password is require!",
-      })
-    }
-
-    new Promise((resolve, reject) => {
-      this.User.findOne({ _id: id }, function (err, user) {
-        if (err) throw err;
+  async register(req, res) {
+    try {
+      const { username, password } = req.body;
+      if (!(username && password)) return res.send(CustomError.UnAuthorizationError());
+  
+      const found_user = await this.User.findOne({ username })
       
-        if (!user) {
-          return reject("user not defined");
-        }
-
-        user.comparePassword(old_password, function(err, isMatch) {
-          if (err) throw err;
-          return isMatch ? resolve(user) : reject("invalid old_password");
-        })
-      }).clone();
-    }).then(async (user) => {
-      if (username) {
-        const found_username = await this.User.findOne({ username }).clone();
-        if (found_username) {
+      if (!found_user) {
+        try {
+          const user = await this.User.create({ username, password })
+          return res.send({
+            status: 200,
+            ok: true,
+            message: "user successfully registered.",
+            data: user
+          });
+        } catch (error) {
+          console.log(error);
           return res.send({
             status: 400,
             ok: false,
-            message: `"${username}" already taken.`,
-          })
+            error,
+            data: {}
+          })  
         }
       }
-
-      const updated_user = await this.User.findOneAndUpdate({ _id: id }, { username, password }).clone();
-      return res.send({
-        status: 200,
-        ok: true,
-        data: updated_user,
-      })
-    }).catch((err) => {
+  
       return res.send({
         status: 400,
         ok: false,
-        error: err,
+        message: `"${username}" is already sign in`,
+        user: {}
       })
-    })
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params
+      if (!id) return res.send("id not defined");
+      const { username, password, old_password } = req.body
+      
+      if (!old_password) {
+        return res.send({
+          status: 400,
+          ok: false,
+          message: "old_password is require!",
+        })
+      }
+  
+      new Promise((resolve, reject) => {
+        this.User.findOne({ _id: id }, function (err, user) {
+          if (err) throw err;
+        
+          if (!user) {
+            return reject("user not defined");
+          }
+  
+          user.comparePassword(old_password, function(err, isMatch) {
+            if (err) throw err;
+            return isMatch ? resolve(user) : reject("invalid old_password");
+          })
+        }).clone();
+      }).then(async (user) => {
+        if (username) {
+          const found_username = await this.User.findOne({ username }).clone();
+          if (found_username) {
+            return res.send({
+              status: 400,
+              ok: false,
+              message: `"${username}" already taken.`,
+            })
+          }
+        }
+  
+        const updated_user = await this.User.findOneAndUpdate({ _id: id }, { username, password }).clone();
+        return res.send({
+          status: 200,
+          ok: true,
+          data: updated_user,
+        })
+      }).catch((err) => {
+        return res.send({
+          status: 400,
+          ok: false,
+          error: err,
+        })
+      })
+    } catch (error) {
+      next(error);
+    }
   }
 
   async delete(req, res) {
-    const { id } = req.params
+    try {
+      const { id } = req.params
 
-    const user = await this.User.findOne({ _id: id }).clone()
+      const user = await this.User.findOne({ _id: id }).clone()
+      
+      if (!id) return res.send({
+        status: 400,
+        ok: false,
+        message: "id is require!"
+      })
     
-    if (!id) return res.send({
-      status: 400,
-      ok: false,
-      message: "id is require!"
-    })
-  
-    if (!user) return res.send({
-      status: 403,
-      ok: false,
-      error: { message: "user not found!" }
-    })
-  
+      if (!user) return res.send({
+        status: 403,
+        ok: false,
+        error: { message: "user not found!" }
+      })
     
-    const deleted_user = await this.User.findOneAndDelete({ _id: id });
-    deleted_user.password = undefined
-  
-    return res.send({
-      status: 201,
-      ok: true,
-      message: "user successfully deleted!",
-      data: deleted_user,
-    })
+      
+      const deleted_user = await this.User.findOneAndDelete({ _id: id });
+      deleted_user.password = undefined
+    
+      return res.send({
+        status: 201,
+        ok: true,
+        message: "user successfully deleted!",
+        data: deleted_user,
+      })
+    } catch (error) {
+      next(error);
+    }
   }
 }
