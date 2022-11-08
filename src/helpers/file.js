9ce168f1513bbda25a979aa456c399675a1ecbe3
Islom Abdurakhmonov <islomabdurakhmonov8@gmail.com>
2022-11-08 21:44:51 +0500

    AppendErrorToFile is created and ended version 1.0

--- src/helpers/file.js
+++ src/helpers/file.js
@@ -1,10 +1,13 @@
 import Fs from 'fs/promises';
+import Path from 'path';
 
 export async function CreateOrAppendFile (path, data) {
   try {
-    const data = Fs.appendFile(path, data, {
+    await Fs.appendFile(path, data, {
       encoding: "utf-8",
     })
+
+    return data;
   } catch (error) {
     throw new Error(error);
   }
@@ -27,6 +30,20 @@ export async function ReadFile(path) {
     const data = await Fs.readFile(path, {
       encoding: "utf-8",
     });
+
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export async function AppendErrorToFile(filename, error) {
+  try {
+    const error_file_path = Path.join(process.cwd(), 'src', 'logs', 'errors');
+
+    await Fs.appendFile(error_file_path, error + '\n', {
+      encoding: "utf-8",
+    })
 
     return data;
   } catch (error) {
